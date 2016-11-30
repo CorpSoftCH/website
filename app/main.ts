d4569be304e333cfdbf8904b1b618e6dd0dcd319
Thomas von Mentlen <Thomas von Mentlen>
2016-11-30 10:38:12 +0100

    delete some things

--- app/main.ts
+++ app/main.ts
@@ -2,8 +2,6 @@ import {bootstrap} from '@angular/platform-browser-dynamic';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {provideRouter} from '@angular/router';
 import {HTTP_PROVIDERS} from '@angular/http';
-//import {Ng2PageScrollModule} from 'ng2-page-scroll/ng2-page-scroll';
-
 //import {theRoutes} from 'app/routing/routing';
 
 import CosoComponent from './components/coso'; 
@@ -11,7 +9,6 @@ import CosoComponent from './components/coso';
 
 bootstrap(CosoComponent, [
 	HTTP_PROVIDERS,
-//	Ng2PageScrollModule,
 	//provideRouter(theRoutes),
 	{provide: LocationStrategy, useClass: HashLocationStrategy}
 ]);
