28ca49d9d6980754f0c401fe1e96531ccc0c2ee0
Thomas von Mentlen <Thomas von Mentlen>
2016-11-30 10:58:34 +0100

    smoothScroll

--- app/components/header.ts
+++ app/components/header.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 
+//import {WINDOW, WINDOW_PROVIDERS} from './window.service';
+
 @Component({
   selector: 'coso-header',
   templateUrl: 'app/templates/header.html',
@@ -9,8 +11,12 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
 export default class HeaderComponent implements OnInit {
 	//private WIDTH_BY_NAVCHANGE: number = 1200;
 
+	win: Window;
+    private offSet: number = 60;
+
 	constructor() {
 		this.startHeaderListener();
+		this.win = window;
 	}
 
 	ngOnInit(): void {
@@ -47,4 +53,54 @@ export default class HeaderComponent implements OnInit {
 			$("#navigation").removeClass("small hide");
 		}
 	}*/
+
+	scrollTo(yPoint: number, duration: number) {
+		setTimeout(() => {
+			this.win.window.scrollTo(0, yPoint- this.offSet);
+		}, duration);
+		return;
+	}
+    smoothScroll(eID) {
+		var startY = this.currentYPosition();
+		var stopY = this.elmYPosition(eID);
+		var distance = stopY > startY ? stopY - startY : startY - stopY;
+		if (distance < 100) {
+			this.win.window.scrollTo(0, stopY); return;
+		}
+		var speed = Math.round(distance / 100);
+		if (speed >= 20) speed = 20;
+		var step = Math.round(distance / 100);
+		var leapY = stopY > startY ? startY + step : startY - step;
+		var timer = 0;
+		if (stopY > startY) {
+			for (var i = startY; i < stopY; i += step) {
+				this.scrollTo(leapY, timer * speed);
+				leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+			} return;
+		}
+		for (var i = startY; i > stopY; i -= step) {
+			this.scrollTo(leapY, timer * speed);
+			leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+		}
+	}
+
+	currentYPosition() {
+		// Firefox, Chrome, Opera, Safari
+		if (self.pageYOffset) return self.pageYOffset;
+		// Internet Explorer 6 - standards mode
+		if (document.documentElement && document.documentElement.scrollTop)
+			return document.documentElement.scrollTop;
+		// Internet Explorer 6, 7 and 8
+		if (document.body.scrollTop) return document.body.scrollTop;
+		return 0;
+	}
+	elmYPosition(eID) {
+		var elm = document.getElementById(eID);
+		var y = elm.offsetTop;
+		var node = elm;
+		while (node.offsetParent && node.offsetParent != document.body) {
+			node = node.offsetParent;
+			y += node.offsetTop;
+		} return y;
+	}
 }
