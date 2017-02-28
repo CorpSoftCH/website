64599726c362c3f1808459e30448ceea85acc40b
Thomas von Mentlen <Thomas von Mentlen>
2017-02-28 16:52:56 +0100

    verschiebungen fixed :D

--- app/components/item.ts
+++ app/components/item.ts
@@ -13,9 +13,11 @@ export class ItemComponent {
 
 	//Als Input wird der Name der Items übergeben, und ob zu beginn nur die ersten oder schon Alle Items anggezeigt werden sollen.
     @Input() contentName: string;
-	@Input() showOnlyTopItems: boolean;
+	@Input() showOnlyTopItems: Array<boolean>;
 	@Input() itemRows: number;
     
+	XTRA_LARGE: number = 1200;
+	LARGE: number = 992;
 	TABLET: number =  768;
 	MOBILE_LARGE: number = 640;
 	MOBILE: number = 480;
@@ -61,11 +63,92 @@ export class ItemComponent {
 		this.fixHoehe();
 
 	}
+	/**
+	 * Nachdem der Kontent initialisiert wurde, werden dort, wo nur die TopItems dargestellt werden Buttens sichtbar gemacht,
+	 * um die anderen Items sichtbar machen zu können.
+	 */
+	private ngAfterContentChecked() {
+		this.setItemsPerRow(window.innerWidth);		
+	}
 
-	fixHoehe() {
+	private showDetailAngebote(i:any, index: number) {
+		
+		let nextRightItem = this.nextRightElement(index);
+		$("#angebote-details").remove();
+		$("#item-" + nextRightItem.id).after("<div class='col-xs-12' id='angebote-details'></div>");
+		$(".arrow").addClass("hide");
+		$("#arrow-" + i.id ).toggleClass("hide");
+
+		if(i.isActive()) {
+			i.setActive(false);
+		} else {
+			i.setActive(true);
+			$("#angebote-details").append("<div id='text-" + i.id + "'></div>");
+
+			for(let para of i.contentStrings) {
+				$("#text-" + i.id).append("<p>" + para + "</p>");
+			}
+			$("#angebote-details").append("<ul id='ul-1-" + i.id + "' class='col-sm-4'></ul>");
+			$("#angebote-details").append("<ul id='ul-2-" + i.id + "' class='col-sm-4'></ul>");
+			$("#angebote-details").append("<ul id='ul-3-" + i.id + "' class='col-sm-4'></ul>");
+			
+			$("#ul-1-" + i.id).append("<p class='ueberschrift'>Wir helfen Ihnen bei:</p>");
+			for(let li of i.specialStrings) {
+				$("#ul-1-" + i.id).append("<li><p>" + li + "</p></li>");
+			}
+			$("#ul-2-" + i.id).append("<p class='ueberschrift'>Profitieren Sie von:</p>");
+			for(let li of i.specialStrings2) {
+				$("#ul-2-" + i.id).append("<li><p>" + li + "</p></li>");
+			}
+			$("#ul-3-" + i.id).append("<p class='ueberschrift'>Wir arbeiten mit:</p>");
+			for(let li of i.specialStrings3) {
+				$("#ul-3-" + i.id).append("<li><p>" + li + "</p></li>");
+			}
+		}
+	}
+
+	private showDetailTeam(i:any, index: number) {
+		let nextRightItem = this.nextRightElement(index);
+		$("#team-details").remove();
+		$("#item-" + nextRightItem.id).after("<div class='col-xs-12' id='team-details'></div>");
+		$(".arrow").addClass("hide");
+		$("#arrow-" + i.id ).toggleClass("hide");
+
+		if(i.isActive()) {
+			i.setActive(false);
+		} else {
+			i.setActive(true);
+			$("#team-details").append("<div id='text-" + i.id + "'></div>");
+			for(let para of i.contentStrings) {
+				$("#text-" + i.id).append("<p>" + para + "</p>");
+			}
+
+			if(index < this.showItems.length - 1) {
+				$("#team-details").append(`
+					<ul id='ul-"` + i.id + `"'>
+						<li><a href="mailto:` + i.specialStrings[0] + `">Email an ` + i.titles[0] + `</a></li>
+						<li><a href="` + i.specialStrings[1] + `">vCard von  ` + i.titles[0] + `</a></li>
+					</ul>`
+				);
+			}
+		}
+	}
+
+	private nextRightElement(index: number): any {
+		if(index < this.showItems.length - 1) {
+			if(this.showItems[index].isRightElement()) {
+				return this.showItems[index];
+			} else {
+				return this.nextRightElement(index + 1);
+			}
+		} else {
+			return this.showItems[index];
+		}
+	}
+
+	private fixHoehe() {
 		var prd = $(".produkt .text");
       prd = [...prd];
-	  console.log(prd);
       var height = 0;
       for(let e of prd) {
         if (e.offsetHeight > height) {
@@ -76,58 +159,55 @@ export class ItemComponent {
         e.style.height = height + "px";
       }
 	}
-	/**
-	 * Nachdem der Kontent initialisiert wurde, werden dort, wo nur die TopItems dargestellt werden Buttens sichtbar gemacht,
-	 * um die anderen Items sichtbar machen zu können.
-	 */
-	private ngAfterContentChecked() {
-		this.setItemsPerRow(window.innerWidth);		
-	}
-
-
 
 	private goToTop() {
 		$('html,body').scrollTop(0);
 	}
 
+	
+	
 	/**
 	 * Beim aktivieren eines Items werden zuerst alle anderen deaktiviert, damit der Fokus immer nur auf einem Item bleibt.
 	 */
-    private deactivateOthers(elements: Array<any> , ignoreElement: any) { //Das aktuelle Element darf noch nicht deaktiviert werden, damit der User es deaktivieren kann.
+    /*private deactivateOthers(elements: Array<any> , ignoreElement: any) { //Das aktuelle Element darf noch nicht deaktiviert werden, damit der User es deaktivieren kann.
+		console.log(ignoreElement.isRightElement());
 		for(let value of elements) {
 			if(value.isActive() && value != ignoreElement) {
 				this.changeView(value, 0);
 			}
 		}
-	}
+	}*/
+
+
 	/**
 	 * Hier werden die Items per Reihe gesetzt.
 	 */
 	private setItemsPerRow(size: number) {
-		var value: number;
-		if (size > this.TABLET && this.itemRows == 3) {
-			value = 3;
-		} else {
-			value = 4;
-		}
-		if (size < this.TABLET) {
-			value = 2;
-		}
-		if (size <= this.MOBILE) {
-			value = 1;
-		}
-
 		for(let element of this.allItems) {
 			if(this.itemRows == 4){
-				$("#item-" + element.id ).addClass("col-sm-6 col-lg-3");
+				$("#item-" + element.id ).addClass("col-sm-6 col-md-3");
+				if (size > this.LARGE) {
+					this.updateRightElements(4);
+				} else if (size > this.TABLET) {
+					this.updateRightElements(2);
+				} else {
+					this.updateRightElements(1);
+				}
 			} else if (this.itemRows == 3) {
 				$("#item-" + element.id ).addClass("col-md-4");
+				this.updateRightElements(3);
 			} else if (this.itemRows == 6) {
 				$("#item-" + element.id ).addClass("col-xs-3 col-sm-2 col-md-2");
+				if (size > this.TABLET) {
+					this.updateRightElements(6);
+				} else {
+					this.updateRightElements(4);
+				}
+				
 			}
 		}
 
-		this.updateRightElements(value);
+		
   	}
 	/**
 	 * Hier werden die sich am rechten Rand befindenden Items markiert.
@@ -149,7 +229,7 @@ export class ItemComponent {
 	 * @element welches Element das geändert wird.
 	 * @index das wievielte Element es ist.
 	 */
-    private changeView(element:any, index:number): void  {
+    /*private changeView(element:any, index:number): void  {
 		if(element.isActive()) {
 			element.setActive(false);
 		} else {
@@ -159,35 +239,9 @@ export class ItemComponent {
 		$("#text-" + element.id ).toggleClass("hide");
 		$("#arrow-" + element.id ).toggleClass("hide");
 		$("#item-" + element.id ).toggleClass("active");
-
-		/*if(element.isActive() && !element.isRightElement()) {
-			/*$("#item-" + element.id ).addClass("col-sm-8");
-			$("#item-" + element.id ).removeClass("col-xs-6 col-sm-4");
-			$("#field-" + element.id ).addClass("col-xs-6");
-			$("#text-" + element.id ).addClass("col-xs-6 moveLeftShow");
-		} else if(element.isActive() && element.isRightElement() && this.itemRows > 1) {
-			//letztem Element wird die Position nicht absolute (Text ragt nicht über die Section)
-			if (element != this.showItems[this.showItems.length -1]) {
-				$("#text-" + element.id ).addClass("rightElement");
-
-			}
-			if(index < this.showItems.length - this.itemRows) {
-				var spezEle = this.showItems[index-1+this.itemRows];
-				$("#item-" + spezEle.id).addClass("special"); 
-			}
-			$("#text-" + element.id ).addClass("moveDownShow");
-		} else {
-			/*$("#item-" + element.id ).removeClass("col-sm-8");
-			$("#item-" + element.id ).addClass("col-xs-6 col-sm-4");
-			$("#field-" + element.id ).removeClass("col-xs-6");
-			$("#text-" + element.id ).removeClass("col-xs-6");
-			
-
-			$(".item").removeClass("special");
-			$(".text").removeClass("rightElement");
-
-		}*/
-    }
+		
+		
+    }*/
 	/**
 	 * Das Wechseln von wenigen Items zu allen Items
 	 */
@@ -204,8 +258,5 @@ export class ItemComponent {
 		$(".moreItems").toggleClass("hide");
 		$(".lessItems").toggleClass("hide");
 	}
-
-	
-
 }
 
